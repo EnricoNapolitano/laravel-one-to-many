ef05a7c23bd0cb5684ef0acdbf37e17713c8b4b2
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-03-12 11:27:48 +0100

    ProjectSeeder: automatic generation of type_id

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -5,7 +5,9 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Str;
+use Illuminate\Support\Arr;
 use App\Models\Project;
+use App\Models\Type;
 use Faker\Generator as Faker;
 
 class ProjectSeeder extends Seeder
@@ -15,11 +17,12 @@ class ProjectSeeder extends Seeder
      */
     public function run(Faker $faker): void
     {
+
         for($i=0; $i<10; $i++){
             $project = new Project();
             $project->title = $faker->text(50);
             $project->content = $faker->paragraphs(30, true);
-            // $project->image = $faker->imageUrl(960, 540);
+            $project->type_id = rand(1, 4);
             $project->slug = Str::slug($project->title, '-');
             $project->save();
         }
