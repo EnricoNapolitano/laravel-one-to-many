81b93a04669f7d848bd11fad7dc0b608da4ac31d
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-03-12 11:30:17 +0100

    added class container and removed btn (edit view)

--- resources/views/admin/projects/edit.blade.php
+++ resources/views/admin/projects/edit.blade.php
@@ -1,12 +1,13 @@
 @extends('layouts.app')
 @section('content')
 
-@include('includes.admin.form')
+<div class="container">
+    
+    <h1 class="mt-5">PROJECT</h1>
+    <h2>{{$project->title}}</h2>
 
-<form action="{{ route('admin.projects.destroy', $project->id) }}" method="POST" class="container">
-@csrf
-@method('DELETE')
-    <button class="btn btn-warning mt-2" onclick="return confirm('Are you sure you want to delete this project?')"><i class="fa-solid fa-trash-can"></i></button>
-</form>
+    @include('includes.admin.form')
+
+</div>
 
 @endsection
